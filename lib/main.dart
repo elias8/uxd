4cebc8dd0e59dcacbab8d01acbc749edfaf32a7e
Elias Andualem <eliasandualem8@gmail.com>
2022-01-25 00:18:41 +0300

    chore: Added `bitsdojo_window` dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 
 import 'application/application.dart';
 
 void main() {
   runApp(const Application());
+  setDesktopInitialConfiguration();
+}
+
+/// Sets the initial desktop configuration setup.
+void setDesktopInitialConfiguration() {
+  doWhenWindowReady(() {
+    appWindow.minSize = const Size(640, 560);
+    appWindow.show();
+  });
 }
